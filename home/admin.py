a11b5495b74093d820b7ce77617c0115b436dac6
@vishalkumar7986 <vishal.kumar@aeccglobal.io>
2024-12-02 18:45:38 +0530

    working with serializers

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Person,Color
+from django.apps import apps
 
-# Register your models here.
+# Register your models here. manual process
+# admin.site.register(Person)
+# admin.site.register(Color)
+
+# Register your models here. Automated process
+# Get all models in the current app
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass  # Skip already registered models
--- home/serializers.py
+++ home/serializers.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
-from .models import Person
+from .models import Person,Color
+import re as regex
+
+
+class ColorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Color
+        fields = ['color_name', 'id']
+
 
 class PeopleSerializer(serializers.ModelSerializer):
+    color = ColorSerializer()
+    color_info = serializers.SerializerMethodField()
 
     class Meta:
         model = Person
         fields = '__all__' # for all fields
+        # depth = 1
         # fields = ['name', 'age'] # for only given fields
         # exclude = ['id'] # for excluding fields
+
+    def get_color_info(self, data):
+        color_obj = Color.objects.get(id= data.color.id)
+
+        return {"color_name": color_obj.color_name, "hex_code": '#000'}
+
+    def validate(self, data):
+        unique_regex =  '^[a-zA-Z\s]+$'
+        print(data['name'])
+        if not regex.match(unique_regex, data['name']):
+            raise serializers.ValidationError('Name should contain only letters and spaces')
+
+
+        if data['age'] < 18:
+            raise serializers.ValidationError('Age should be greater then 18')
+        return data
